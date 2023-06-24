db333a316d5de692d1e3491e66e8b0252e0e0b42
Jihun Park <jihunpark0989@gmail.com>
2023-06-24 10:42:40 +0100

    Drag and drop functionality example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import "./App.css";
 function App() {
-  return <></>;
+  const [widgets, setWidgets] = useState([]);
+  //event handlers
+  function handleOnDrag(e, widgetType) {
+    e.dataTransfer.setData("widgetType", widgetType);
+  }
+  function handleOnDrop(e) {
+    const widgetType = e.dataTransfer.getData("widgetType");
+    setWidgets([...widgets, widgetType]);
+  }
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="App">
+      <div className="widgets">
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget A")}
+        >
+          Widget A
+        </div>
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget B")}
+        >
+          Widget B
+        </div>
+        <div
+          className="widget"
+          draggable
+          onDragStart={(e) => handleOnDrag(e, "Widget C")}
+        >
+          Widget C
+        </div>
+      </div>
+      <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
+        {widgets.map((widget, index) => {
+          return (
+            <div className="dropped-widget" key={index}>
+              {widget}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
