765a6430be9d28a5b95f18deaa61ca288954fcd8
Adrian <k2113211@kingston.ac.uk>
2023-07-07 20:00:06 +0100

    removed className from Coursemate card, fav card, searchbar and favourites

--- src/components/CoursemateCard.js
+++ src/components/CoursemateCard.js
@@ -2,14 +2,13 @@ import Card from "./UI/Card";
 export default function CoursemateCards(props) {
   return (
     <Card>
-      <img className=" " alt="Img of student" src={props.img} />
-      <h1 className="">{props.name}</h1>
-      <h1 className="">Lorem ipsum dolor sit amet, consectetur</h1>
+      <img alt="Img of student" src={props.img} />
+      <h1>{props.name}</h1>
+      <h1>Lorem ipsum dolor sit amet, consectetur</h1>
       <button
         onClick={() => {
           props.addToFavourites(props.id);
         }}
-        className=""
       >
         <span>Add Favourite</span>
       </button>
--- src/components/FavouriteCard.js
+++ src/components/FavouriteCard.js
@@ -2,14 +2,13 @@ import Card from "./UI/Card";
 export default function FavouriteCard(props) {
   return (
     <Card>
-      <img className="" alt="Img of student" src={props.img} />
-      <h1 className="">{props.name}</h1>
-      <h1 className="">Lorem ipsum dolor sit amet, consectetur</h1>
+      <img alt="Img of student" src={props.img} />
+      <h1>{props.name}</h1>
+      <h1>Lorem ipsum dolor sit amet, consectetur</h1>
       <button
         onClick={() => {
           props.removeFavourite(props.id);
         }}
-        className=""
       >
         <span>Remove Favourite</span>
       </button>
